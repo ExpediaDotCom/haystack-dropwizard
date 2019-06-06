bf4d4159640c267db635bdaae167f64ce7011c98
Andrew McGhie <andrew.mcghie@gmail.com>
2019-06-06 12:44:42 +1000

    #9: Introduce the ability to specify an IdGenerator in the TracerFactory configuration as well as customizing ServerTracingDynamicFeature via the HaystackTracerBundle constructor

--- src/main/java/com/expedia/haystack/dropwizard/bundle/HaystackTracerBundle.java
+++ src/main/java/com/expedia/haystack/dropwizard/bundle/HaystackTracerBundle.java
@@ -16,6 +16,9 @@
  */
 package com.expedia.haystack.dropwizard.bundle;
 
+import com.expedia.haystack.dropwizard.jackson.IdGeneratorDeserializer;
+import com.expedia.www.haystack.client.idgenerators.IdGenerator;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -25,20 +28,49 @@ import io.opentracing.contrib.jaxrs2.server.ServerTracingDynamicFeature;
 import io.opentracing.contrib.jaxrs2.server.SpanFinishingFilter;
 import io.opentracing.noop.NoopTracerFactory;
 import java.util.EnumSet;
+import java.util.function.Function;
 import javax.servlet.DispatcherType;
 import org.apache.commons.lang3.Validate;
 
 public class HaystackTracerBundle<T extends Traceable> implements ConfiguredBundle<T> {
+
+    private final IdGeneratorDeserializer idGeneratorDeserializer;
+    private final Function<Tracer, ServerTracingDynamicFeature.Builder> serverTracingBuilder;
+
+    public HaystackTracerBundle() {
+        this(new IdGeneratorDeserializer(), ServerTracingDynamicFeature.Builder::new);
+    }
+
+    /**
+     * Allow for registration of a customized {@link IdGeneratorDeserializer} and/or
+     * {@link ServerTracingDynamicFeature.Builder}.
+     *
+     * <p>Customizing the {@link IdGeneratorDeserializer} is necessary if you've written your own {@link IdGenerator}.</p>
+     * <p>Customizing the {@link ServerTracingDynamicFeature.Builder} is necessary if you want to use anything other than
+     * the standard spanDecorators, serializationSpanDecorators, skipPattern etc</p>
+     *
+     * @param idGeneratorDeserializer a customized {@link IdGeneratorDeserializer}
+     * @param serverTracingBuilder a customized {@link ServerTracingDynamicFeature.Builder}
+     */
+    public HaystackTracerBundle(IdGeneratorDeserializer idGeneratorDeserializer,
+                                Function<Tracer, ServerTracingDynamicFeature.Builder> serverTracingBuilder) {
+        this.idGeneratorDeserializer = idGeneratorDeserializer;
+        this.serverTracingBuilder = serverTracingBuilder;
+    }
 
     @Override
     public void run(T traceable, Environment environment) {
         Validate.notNull(traceable);
         Validate.notNull(environment);
 
+        environment
+                .getObjectMapper()
+                .registerModule(new SimpleModule().addDeserializer(IdGenerator.class, idGeneratorDeserializer));
+
         final Tracer tracer = traceable.getTracerFactory().build(environment);
 
-        final ServerTracingDynamicFeature tracingDynamicFeature = new ServerTracingDynamicFeature
-                .Builder(tracer)
+        final ServerTracingDynamicFeature tracingDynamicFeature = serverTracingBuilder
+                .apply(tracer)
                 .withTraceSerialization(false).build();
         environment.jersey().register(tracingDynamicFeature);
 
